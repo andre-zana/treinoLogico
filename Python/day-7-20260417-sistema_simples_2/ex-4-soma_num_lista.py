9b2472c7b31c73f12442c118cc348f8241a2854e
André Zana <andrezana@hotmail.com>
2026-04-17 20:09:48 -0300

    ex 4

---
+++ Python/day-7-20260417-sistema_simples_2/ex-4-soma_num_lista.py
@@ -0,0 +1,84 @@
+numLista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def ver_lista(lista):
+  return lista
+
+def ver_maior(lista):
+  maiorNum = lista[0]
+  for num in lista:
+      if num > maiorNum:
+        maiorNum = num    
+  return maiorNum
+  
+def ver_media(lista):
+  somaLista = 0
+  for num in lista:
+    somaLista += num # ou somaLista = somaLista + num
+  return somaLista / len(lista)
+
+def add_numero(lista, numero):
+  lista.append(numero)
+  return numero
+
+def del_numero(lista, numero):
+  if numero in lista:
+    lista.remove(numero)  
+    return True
+  else:
+    return False
+  
+def ver_menor(lista):
+  menorNum = lista[0]
+  for num in lista:
+      if num < menorNum:
+        menorNum = num    
+  return menorNum
+
+def ver_soma(lista):
+  somaLista = 0
+  for num in lista:
+    somaLista += num # ou somaLista = somaLista + num
+  return somaLista
+
+while True:
+  print("")
+  print("Bem-vindo ao sistema simples!")
+  print("")
+  print("De acordo com o menu abaixo...")
+  print("1 - Ver lista")
+  print("2 - Ver maior número")
+  print("3 - Ver média")
+  print("4 - Adicionar número")
+  print("5 - Remover número")
+  print("6 - Ver menor número")
+  print("7 - Ver soma")
+  print("8 - Sair")
+  userInput = input("Digite o número da opção desejada: ")
+  print("")
+
+  if userInput == "1":
+    print(ver_lista(numLista))
+  elif userInput == "2":
+    print("O maior número da lista é: ",ver_maior(numLista))
+  elif userInput == "3":
+    print("A média dos números da lista é: ",ver_media(numLista))
+  elif userInput == "4":
+    addNum = int(input("Digite o número que deseja adicionar a lista: "))
+    print(f"O número {add_numero(numLista, addNum)} foi adicionado a lista com sucesso!")
+  elif userInput == "5":
+    delNum = int(input("Digite o número que deseja remover da lista: "))
+    deletado = del_numero(numLista, delNum)
+    if deletado:
+      print("")
+      print(f"O número {delNum} foi removido da lista com sucesso!")
+    else:
+      print("")
+      print(f"O número {delNum} não foi encontrado na lista!")
+  elif userInput == "6":
+    print("O menor número da lista é: ",ver_menor(numLista))
+  elif userInput == "7":
+    print("A soma dos números da lista é: ",ver_soma(numLista))
+  elif userInput == "8":
+    break
+  else:
+    print("Opção inválida! Por favor, escolha uma opção do menu.")
